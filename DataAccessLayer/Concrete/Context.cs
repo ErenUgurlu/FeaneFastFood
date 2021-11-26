7fff06a7ecf5ae0a4f2174eed9a86448dfee8a1f
ErenUgurlu <eren.ugurlu@ogr.sakarya.edu.tr>
2021-11-26 15:20:46 +0300

    Add project files.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=localhost;database=FeaneFastFoodDb; integrated security=true;");
+
+        }
+    }
+}
