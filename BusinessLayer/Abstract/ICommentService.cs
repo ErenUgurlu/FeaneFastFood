d7e539eb77c2e8278fb9d59911eb2ae5aba6c980
Saadet-T <lzzaplzzap123@gmail.com>
2021-12-09 11:23:19 +0300

    Comment Edit and User adding

---
+++ BusinessLayer/Abstract/ICommentService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    interface ICommentService
+    {
+        void Add(Comment comment);
+        void Update(Comment comment);
+        void Delete(Comment comment);
+        List<Comment> GetAll();
+        Comment GetById(Guid id);
+    }
+}
