3a1034863dc68268a34142cc42c9046dfcaab1c5
Saadet-T <lzzaplzzap123@gmail.com>
2021-12-18 23:07:09 +0300

    Discount,Order and Order Detail Services and Managers

---
+++ BusinessLayer/Concrete/DiscountManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class DiscountManager : IDiscountService
+    {
+        readonly IDiscountDal _discountDal;
+
+        public DiscountManager(IDiscountDal discountDal)
+        {
+            _discountDal = discountDal;
+        }
+
+        public void Add(Discount discount)
+        {
+            _discountDal.Insert(discount);
+        }
+
+        public void Delete(Discount discount)
+        {
+            _discountDal.Delete(discount);
+        }
+
+        public void Update(Discount discount)
+        {
+            _discountDal.Update(discount);
+        }
+
+        public List<Discount> GetAll()
+        {
+            return _discountDal.GetListAll();
+        }
+
+        public Discount GetById(Guid id)
+        {
+            return _discountDal.GetById(id);
+        }
+    }
+}
